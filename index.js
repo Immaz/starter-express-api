57409ef950be830bf13922986e886635c969a4a7
Immaz <76837602+Immaz@users.noreply.github.com>
2023-09-16 22:23:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send(CYCLIC_URL)
+    res.send(process.env.CYCLIC_URL)
 })
 app.listen(process.env.PORT || 3000)
